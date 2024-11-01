6e2c8550f2e9bb3227ff6ea355ae2b2dd27a676a
Zhalhami <73404711+Zhalhami@users.noreply.github.com>
2024-11-01 03:27:16 +0100

    Intial commit

---
+++ src/components/TeamMessage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MessageSimple, useMessageContext } from 'stream-chat-react';
+
+const TeamMessage = () => {
+    const { message } = useMessageContext();
+
+    return (
+        <MessageSimple
+            message={{ ...message, user: {}}}
+        />
+    )
+}
+
+export default TeamMessage
